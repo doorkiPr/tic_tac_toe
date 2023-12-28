b583fca50fce4850b0b031e181119f7dee513d9a
doorkiPr <boumarafchakib7@gmail.com>
2023-12-28 04:51:52 +0100

    ADD: add gameState factory and checkwin function

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ const gameBoard = (() => {
   ];
   return {};
 })();
+
+const gameState = (() => {
+  function checkWin(array) {
+
+    for (let i = 0; i < array.length; i++) {
+      //check if rows are equals and not empty
+      if (array[i][0] !== "" && array[i][0] === array[i][1] && array[i][2] === array[i][2]) {
+        return true
+      }
+      //check collumns are equals and not empty
+      if (array[0][i] !== "" && array[0][i] === array[1][i] && array[1][i] === array[2][i]) {
+        return true
+      }
+      //check if diagonals are equals and not empty
+      if (array[0][0] !== "" && array[0][0] === array[1][1] && array[1][1] === array[2][2]) {
+        return true
+      }
+      if (array[0][2] !== "" && array[0][2] === array[1][1] && array[1][1] === array[2][0]) {
+        return true
+      }
+    }
+
+    return false
+
+  }
+  return {};
+})();
+
